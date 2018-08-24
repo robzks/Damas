f1d78fc793533722d2d514eeca943d0af2f972d0
Robson <robzks@hotmail.com>
2018-08-24 10:22:27 -0300

    criando classe PosicaoDama

--- DamasConsole/Program.cs
+++ DamasConsole/Program.cs
@@ -6,24 +6,26 @@ using tabuleiro.Excessao;
 namespace DamasConsole {
     class Program {
         static void Main(string[] args) {
-            try {
-                Tabuleiro tab = new Tabuleiro(8, 8);
-                tab.colocarPeca(new PecaNormal(tab, Cor.Branco), new Posicao(0, 0));
-                tab.colocarPeca(new PecaNormal(tab, Cor.Branco), new Posicao(0, 1));
-                tab.colocarPeca(new Dama(tab, Cor.Branco), new Posicao(7, 6));
-                tab.colocarPeca(new Dama(tab, Cor.Branco), new Posicao(7, 7));
-                Tela.imprimirTabuleiro(tab);
-                Console.ReadLine();
-                Console.Clear();
-                 tab.colocarPeca(new Dama(tab, Cor.Branco), new Posicao(7, 6));
-                tab.retirarPeca(new Dama(tab, Cor.Branco), new Posicao(7, 7));
-                Tela.imprimirTabuleiro(tab);
-            }
-            catch (TabuleiroException e) {
-                Console.WriteLine(e.Message);
-                
-            }
-           
+            //try {
+            //    Tabuleiro tab = new Tabuleiro(8, 8);
+            //    tab.colocarPeca(new PecaNormal(tab, Cor.Branco), new Posicao(0, 0));
+            //    tab.colocarPeca(new PecaNormal(tab, Cor.Branco), new Posicao(0, 1));
+            //    tab.colocarPeca(new Dama(tab, Cor.Branco), new Posicao(7, 6));
+            //    tab.colocarPeca(new Dama(tab, Cor.Branco), new Posicao(7, 7));
+            //    Tela.imprimirTabuleiro(tab);
+            //    Console.ReadLine();
+            //    Console.Clear();
+            //     tab.colocarPeca(new Dama(tab, Cor.Branco), new Posicao(7, 6));
+            //    tab.retirarPeca(new Dama(tab, Cor.Branco), new Posicao(7, 7));
+            //    Tela.imprimirTabuleiro(tab);
+            //}
+            //catch (TabuleiroException e) {
+            //    Console.WriteLine(e.Message);
+
+            //}
+
+            Posicao p = new PosicaoDama('a', 1).toPosicao();
+            Console.WriteLine(p);
 
             Console.ReadLine();
         }
---
+++ DamasConsole/dama/PosicaoDama.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tabuleiro;
+
+namespace dama {
+    class PosicaoDama {
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public PosicaoDama(char coluna, int linha) {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        public Posicao toPosicao() {
+            return new Posicao(8 - linha , coluna - 'a');
+        }
+
+
+        public override string ToString() {
+            return "" + coluna + linha;
+        }
+
+
+    }
+}
